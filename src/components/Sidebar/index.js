dd8d3ed77486229c57fd5cd1b2a667e303caffca
Anandhan Asahi <anandhan@mail.asahitechnologies.com>
2024-02-13 15:44:36 +0530

    dropdown menu altered

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,9 +3,18 @@ import "./Sidebar.css";
 import DashboardIcon from "../../assets/images/Dashboard.png";
 import AnalyticsIcon from "../../assets/images/Analytics.png";
 import RightArrowWhiteIcon from "../../assets/images/RightArrowWhite.png";
+import AddConnectionIcon from "../../assets/images/AddConnection.png";
+import SavedConnectionIcon from "../../assets/images/SavedConnection.png";
 import { Dropdown } from "react-bootstrap";
+import { useState } from "react";
 
 const Sidebar = () => {
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	const handleDropdownToggle = (isOpen) => {
+		setIsDropdownOpen(isOpen);
+	};
+
 	return (
 		<div className="sidebar-container">
 			<div className="navlink-container">
@@ -13,11 +22,19 @@ const Sidebar = () => {
 					<img src={DashboardIcon} alt="Dashboard" />
 					Dashboard
 				</NavLink>
-				<Dropdown>
+				<Dropdown onToggle={handleDropdownToggle}>
 					<Dropdown.Toggle className="action-container">
 						<img src={AnalyticsIcon} alt="Analytics" />
 						<div style={{ flexGrow: 1 }}>Analytics</div>
-						<img src={RightArrowWhiteIcon} alt="RightArrow" />
+						<img
+							style={{
+								transform: isDropdownOpen
+									? "rotate(90deg)"
+									: "",
+							}}
+							src={RightArrowWhiteIcon}
+							alt="RightArrow"
+						/>
 					</Dropdown.Toggle>
 
 					<Dropdown.Menu className="action-menu-container">
@@ -25,14 +42,14 @@ const Sidebar = () => {
 							className="action-container action-menu"
 							href="#/action-1"
 						>
-							<img src={AnalyticsIcon} alt="Analytics" />
+							<img src={AddConnectionIcon} alt="Analytics" />
 							Add a connection
 						</Dropdown.Item>
 						<Dropdown.Item
 							className="action-container action-menu"
 							href="#/action-2"
 						>
-							<img src={AnalyticsIcon} alt="Analytics" />
+							<img src={SavedConnectionIcon} alt="Analytics" />
 							Saved connections
 						</Dropdown.Item>
 					</Dropdown.Menu>
